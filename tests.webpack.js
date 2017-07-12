a9e54186061822d779d4009bd8d0953f0d7e92d6
Artur Ciocanu <ciocanu@adobe.com>
2017-07-12 13:03:39 +0300

    Removing all the old stuff

--- tests.webpack.js
+++
@@ -1,5 +0,0 @@
-var testsContext = require.context('./src', true, /\.spec\.js$/);
-testsContext.keys().forEach(testsContext);
-
-var srcContext = require.context('./src', true, /^((?!__tests__).)*.js$/);
-srcContext.keys().forEach(srcContext);
